eafa9e3674d4a68729899341f924f1e2449e8e47
Andrew Allen <asallen67@gmail.com>
2019-06-11 16:13:01 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
 
-  const listContainer = document.getElementById("list");
-  const renderApp = () => (listContainer.innerHTML = taskList.render());
+	// VARIABLES
+	const taskForm = document.getElementById('create-task-form')
+	const submitBtn = document.getElementById('new-task-btn')
+  const listDiv = document.getElementById("list")
+  const list = new TaskList()
 
-  // Add Form Behavior Here!
+	// MAIN
+	document.addEventListener('click', handleClickEvents)
+	document.getElementById('sort').addEventListener('change', sort_tasks)
+	list.add(new TaskListItem('Hello, world!'))
+	renderApp()
 
-  renderApp()
-});
+	//											//
+	// FUNCTION DEFINITIONS //
+	//											//
+	function handleClickEvents(e) {
+		// console.log(e.target)
+		if(e.target.id === 'new-task-btn')
+			new_task(e)
+		else if(e.target.className === 'delete-btn')
+			e.target.parentElement.remove()
+	}
+
+	function sort_tasks(e) {
+		list.sort(e.target.value)
+		renderApp()
+	}
+
+  function new_task(e) {
+  	e.preventDefault()
+  	
+  	let newTask = new TaskListItem(taskForm['new-task-description'].value, taskForm['priority'].value)
+  	listDiv.innerHTML += newTask.render()
+
+  	list.add(newTask)
+  	taskForm.reset()
+  }
+
+  function renderApp() {
+  	listDiv.innerHTML = list.render()
+  }
+
+  // END //
+})
--- src/taskList.js
+++ src/taskList.js
@@ -1,3 +1,42 @@
 class TaskList {
-  
+
+	constructor(task_arr=[]) {
+		this.tasks = []
+		this.tasks.concat(task_arr)
+	}
+
+	add(new_task) {
+		this.tasks.push(new_task)
+	}
+
+	sort(sort_method) {
+		if(sort_method === 'alpha')
+			this.tasks = this.tasks.sort(function(a,b){
+			  if (a.item.toLowerCase() < b.item.toLowerCase()) return -1
+			  if (a.item.toLowerCase() > b.item.toLowerCase()) return  1
+			  return 0
+			})
+		else if(sort_method === 'asc_prio')
+			this.tasks = this.tasks.sort(function(a,b){
+			  if (a.priority < b.priority) return -1
+			  if (a.priority > b.priority) return  1
+			  return 0
+			})
+		else if(sort_method === 'desc_prio')
+			this.tasks = this.tasks.sort(function(a,b){
+			  if (a.priority < b.priority) return  1
+			  if (a.priority > b.priority) return -1
+			  return 0
+			})
+	}
+
+	render() {
+		console.log(this.tasks)
+		let html = ''
+		this.tasks.forEach(tli => html += tli.render())
+		return html
+	}
+
+	alphabetical(a, b) {
+	}
 }
